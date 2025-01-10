7a24f4d9ac18e5554560925bf688d53f238f437a
marzaananda <marzaanan21@gmail.com>
2025-01-10 19:47:59 +0700

    fix.eror

--- src/pages/post/PostModal.tsx
+++ src/pages/post/PostModal.tsx
@@ -5,7 +5,7 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import CommentHP from "../../components/CommentHp";
+import CommentHP from "../../components/CommentHP";
 import CommentPC from "../../components/CommentPc";
 
 
